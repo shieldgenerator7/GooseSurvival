15569c0633ec31952f0cf3db7645e2ecbf184b1a
shieldgenerator7 <shieldgenerator7@yahoo.com>
2020-07-11 15:19:49 -0400

    Increase player speed when hitting hazards

---
+++ Assets/Scripts/Hazard.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hazard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //If it hits something that does not move,
+        Rigidbody2D rigidbody2d = collision.gameObject.GetComponent<Rigidbody2D>();
+        if (!rigidbody2d)
+        {
+            //Make it unmovable
+            Destroy(GetComponent<Rigidbody2D>());
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        PlayerController pc = collision.gameObject.GetComponent<PlayerController>();
+        if (pc)
+        {
+            pc.maxMoveSpeed *= 2f;
+            Destroy(gameObject);
+        }
+    }
+}
