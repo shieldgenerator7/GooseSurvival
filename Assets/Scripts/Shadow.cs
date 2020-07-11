7b75a5bdeb0dbbc61272f21249dcdba2fd2c57ed
shieldgenerator7 <shieldgenerator7@yahoo.com>
2020-07-11 13:35:08 -0400

    Shrink shadows when caster is in air

--- Assets/Scripts/Shadow.cs
+++ Assets/Scripts/Shadow.cs
@@ -5,6 +5,30 @@ using UnityEngine;
 public class Shadow : MonoBehaviour
 {
     public GameObject caster;//the object that casts this shadow
+    public float minWidthFactor = 0.1f;//the thinnest it can be when the caster is up really high
+    public float maxForeseeableHeight = 5;
+
+    private Vector3 defaultScale;
+
+    private float _casterHeight = 0;
+    public float CasterHeight
+    {
+        get => _casterHeight;
+        set
+        {
+            _casterHeight = Mathf.Clamp(value, 0, maxForeseeableHeight);
+            Bounds casterBounds = caster.GetComponent<SpriteRenderer>().bounds;
+            Bounds bounds = GetComponent<SpriteRenderer>().bounds;
+            Vector3 scale = defaultScale;
+            scale.x = Mathf.Clamp(
+                defaultScale.x * (1 - (_casterHeight / maxForeseeableHeight)),
+                0.1f,
+                1
+                );
+            scale.y = scale.x * 0.10f;
+            transform.localScale = scale;
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -21,10 +45,11 @@ public class Shadow : MonoBehaviour
             //Set size of shadow
             Bounds casterBounds = caster.GetComponent<SpriteRenderer>().bounds;
             Bounds bounds = GetComponent<SpriteRenderer>().bounds;
-            Vector3 scale = transform.localScale;
-            scale.x = casterBounds.size.x / bounds.size.x;
-            scale.y = scale.x * 0.10f;
-            transform.localScale = scale;
+            defaultScale = transform.localScale;
+            defaultScale.x = casterBounds.size.x / bounds.size.x;
+            defaultScale.y = defaultScale.x * 0.10f;
+            transform.localScale = defaultScale;
+            checkGroundDistance();
         }
         else
         {
@@ -37,6 +62,11 @@ public class Shadow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        checkGroundDistance();
+    }
+
+    void checkGroundDistance()
+    {
         RaycastHit2D[] rch2ds = Physics2D.RaycastAll(caster.transform.position, Vector2.down);
         foreach (RaycastHit2D rch2d in rch2ds)
         {
@@ -45,6 +75,7 @@ public class Shadow : MonoBehaviour
                 if (rch2d.collider.gameObject != caster)
                 {
                     transform.position = rch2d.point;
+                    CasterHeight = caster.transform.position.y - transform.position.y;
                     break;
                 }
             }
